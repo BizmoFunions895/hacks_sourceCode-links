ef630b8dd3f098346a3be726522d4278b1d7e78e
BizmoFunions895 <123105246+BizmoFunions895@users.noreply.github.com>
2023-02-26 16:46:28 -0700

    made by ading 2210

--- edpuzzleCode.js
+++ edpuzzleCode.js
@@ -5,7 +5,7 @@ if (typeof document.dev_env != "undefined") {
 }
 else {
   //get resources off of github to not inflate the jsdelivr stats
-  base_url = "https://raw.githubusercontent.com/BizmoFunions895/hacks_sourceCode-links/main/edpuzzleCode.js";
+  base_url = "https://raw.githubusercontent.com/ading2210/edpuzzle-answers/main";
 }
 
 function http_get(url, callback, headers=[], method="GET", content=null) {
@@ -193,11 +193,8 @@ function openPopup(assignment) {
     <p style="font-size: 12px" id="loading_text"></p>
   </div>
   <hr>
-  <p style="font-size: 12px">Made by: <a target="_blank" 
-  href="https://github.com/BizmoFunions895">BizmoFunions895</a> 
-  on Github | Website: <a target="_blank" href="about:blank">Not Available</a> | 
-  Source code: <a target="_blank" href="https://github.com/BizmoFunions895/Edpuzzle-skip">BizmoFunions895/Edpuzzle-skip</a></p>`;
- 
+  <p style="font-size: 12px">Made by: <a target="_blank" href="https://github.com/ading2210">ading2210</a> on Github | Website: <a target="_blank" href="https://edpuzzle.hs.vc">edpuzzle.hs.vc</a> | Source code: <a target="_blank" href="https://github.com/ading2210/edpuzzle-answers">ading2210/edpuzzle-answers</a></p>
+  <p style="font-size: 12px">Licenced under the <a target="_blank" href="https://github.com/ading2210/edpuzzle-answers/blob/main/LICENSE">GNU GPL v3</a>. Do not reupload or redistribute without abiding by those terms.</p>`;
   popup = window.open("about:blank", "", "width=600, height=400");
   popup.document.write(base_html);
 
